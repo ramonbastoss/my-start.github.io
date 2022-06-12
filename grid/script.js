1dc6aecda41dd01d9ca2f05d59056e20123af6fe
Renan Bastos <renan_bastos@hotmail.com>
2022-06-12 00:22:55 -0300

    Update script.js

--- grid/script.js
+++ grid/script.js
@@ -16,51 +16,45 @@ const firstMarked = document.querySelector(`.box:nth-of-type(${size})`)
 firstMarked.classList.add('marked')
 
 
+
 window.addEventListener('keydown', (e) => {
+    const cyan = document.querySelector('.marked')
     // Left
     if (e.code === 'KeyA' || e.code === 'ArrowLeft') {
-        const cyan = document.querySelector('.marked')
         const leftBrother = cyan.previousElementSibling
         if (leftBrother !== null) {
-            console.log(leftBrother)
             cyan.classList.remove('marked')
             leftBrother.classList.add('marked')
         }
     }
     // Right
     if (e.code === 'KeyD' || e.code === 'ArrowRight') {
-        const cyan = document.querySelector('.marked')
         const rightBrother = cyan.nextElementSibling
         if (rightBrother !== null) {
-            console.log(rightBrother)
             cyan.classList.remove('marked')
             rightBrother.classList.add('marked')
         }
     }
     // Down
     if (e.code === 'KeyS' || e.code === 'ArrowDown') {
-        const cyan = document.querySelector('.marked')
         let downBrother = cyan
         for (i = 1; i <= size; i++) {
             downBrother = downBrother.nextElementSibling
         }
 
         if (downBrother !== null) {
-            console.log(downBrother)
             cyan.classList.remove('marked')
             downBrother.classList.add('marked')
         }
     }
     // Up
     if (e.code === 'KeyW' || e.code === 'ArrowUp') {
-        const cyan = document.querySelector('.marked')
         let upBrother = cyan
         for (i = 1; i <= size; i++) {
             upBrother = upBrother.previousElementSibling
         }
 
         if (upBrother !== null) {
-            console.log(upBrother)
             cyan.classList.remove('marked')
             upBrother.classList.add('marked')
         }
